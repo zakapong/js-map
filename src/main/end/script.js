3f9a5b25c1a01c856aaf693323aba495152df30e
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-09-07 10:54:18 +0200

    Removing other files

--- src/main/end/script.js
+++
@@ -1,9 +0,0 @@
-var gMapAPIKey = 'YOUR_API_KEY_HERE';
-
-var map;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('hplus-map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	});
-}
